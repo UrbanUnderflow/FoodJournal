83925a49adb7d654308093f9f2d2e1d06ac223e8
tdg10e <tremaine.grant@gmail.com>
2023-08-29 17:26:35 -0400

    Initial Commit

---
+++ FoodJournal/FoodJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodJournalApp.swift
+//  FoodJournal
+//
+//  Created by Tremaine Grant on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
